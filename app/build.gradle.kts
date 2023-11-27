9e45f8b69f0aa36d39b8abac2ad0faafd2f34e4a
otarbakh <otarbakh@gmail.com>
2023-11-28 01:02:23 +0400

    navigation setted, and moves one to another layer.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation(project(mapOf("path" to ":book-datasource")))
+    implementation(project(mapOf("path" to ":book-list")))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -75,7 +76,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     implementation("io.github.raamcosta.compose-destinations:core:1.8.42-beta")
-    ksp("io.github.raamcosta.compose-destinations:ksp:1.8.42-be ta")
+    ksp("io.github.raamcosta.compose-destinations:ksp:1.8.42-beta")
 
 
 }
--- app/src/main/java/com/example/multimodulenav/MainActivity.kt
+++ app/src/main/java/com/example/multimodulenav/MainActivity.kt
@@ -3,22 +3,40 @@ package com.example.multimodulenav
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.example.book_datasource.destinations.ProfileScreenDestination
+import com.example.book_datasource.destinations.SecondFragmentDestination
+import com.example.book_list.destinations.FirstFragmentDestination
+import com.example.multimodulenav.destinations.MainScreenDestination
 import com.example.multimodulenav.ui.theme.MultiModuleNavTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.spec.DestinationSpec
+import com.ramcosta.composedestinations.spec.NavGraphSpec
+import com.ramcosta.composedestinations.spec.Route
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val appNavGraph = object : NavGraphSpec {
+            override val route: String = "appNavGraph"
+            override val startRoute: Route = MainScreenDestination
+            override val destinationsByRoute: Map<String, DestinationSpec<*>> = mapOf(
+                "main_screen" to MainScreenDestination,
+                "first_fragment" to FirstFragmentDestination,
+                "second_fragment" to SecondFragmentDestination
+            )
+        }
+
         setContent {
 
             MultiModuleNavTheme {
-                DestinationsNavHost(navGraph = NavGraphs.root)
+                DestinationsNavHost(navGraph = appNavGraph)
 
 
             }
@@ -27,14 +45,16 @@ class MainActivity : ComponentActivity() {
 
 }
 
-@RootNavGraph(start = true) // sets this as the start destination of the default nav graph
+@RootNavGraph(start = true)
 @Destination
 @Composable
-fun HomeScreen(
+fun MainScreen(
     navigator: DestinationsNavigator
 ) {
-    /*...*/
-    navigator.navigate(ProfileScreenDestination(id = 7, groupName = "Kotlin programmers"))
+    Button(
+        onClick = { navigator.navigate(FirstFragmentDestination) }) {
+        Text("Go to First Fragment")
+    }
 }
 
 
