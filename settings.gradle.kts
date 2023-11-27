1321c4fc462f20ca442a5599dba31145f4da9e2f
otarbakh <otarbakh@gmail.com>
2023-11-27 12:06:50 +0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MultiModuleNav"
+include(":app")
+include(":book-datasource")
+include(":book-list")
